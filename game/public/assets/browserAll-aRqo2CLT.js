00522fded36e6c28c2b38ff75f58f819e593284c
Artiishock <79396846+Artiishock@users.noreply.github.com>
2026-03-27 03:09:09 +0100

    tect

---
+++ game/public/assets/browserAll-aRqo2CLT.js
@@ -0,0 +1,1 @@
+import{e as a,A as i,C as s,a as m,E as r,F as d}from"./pixi-Cp5098HU.js";import"./webworkerAll-CyEZ6Pak.js";a.add(i),a.mixin(s,m),a.add(r),a.mixin(s,d);
---
+++ game/public/assets/webworkerAll-CyEZ6Pak.js
@@ -0,0 +1,1 @@
+import{e as a,d,f as s}from"./pixi-Cp5098HU.js";a.add(d),a.add(s);
